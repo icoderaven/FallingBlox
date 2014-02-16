fad076b1d29d8d643eb77ac6d700f5499c763f8b
Kumar Shaurya Shankar <icoderaven@gmail.com>
2014-02-16 18:35:28 -0500

    Integrated random policy generator

--- tetris_899/src/tetris/agent/RandomPolicy.java
+++ tetris_899/src/tetris/agent/RandomPolicy.java
@@ -8,7 +8,7 @@ public class RandomPolicy implements Policy {
 		// return random action
 		int[][] moves =  curr_state.legalMoves();
 		//Get a random move from list of moves nx2
-		int move_index = (int) Math.random()*moves.length;
+		int move_index = (int) (Math.random()*moves.length);
 //		return new Action(moves[move_index][0], moves[move_index][1]) ;
 		return move_index;
 	}
