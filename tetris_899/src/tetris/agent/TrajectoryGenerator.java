fb18db7f33c58f7682d966ac6ae9af09a3115ab9
Humhu <humphrey.hu@gmail.com>
2014-02-21 23:12:17 -0500

    Added threaded trajectory generator.

--- tetris_899/src/tetris/agent/TrajectoryGenerator.java
+++ tetris_899/src/tetris/agent/TrajectoryGenerator.java
@@ -1,4 +1,6 @@
 package tetris.agent;
+
+import java.util.concurrent.Callable;
 import tetris.simulator.State;
 
 /** 
@@ -16,7 +18,7 @@ import tetris.simulator.State;
  * @author Humhu
  *
  */
-public abstract class TrajectoryGenerator {
+public abstract class TrajectoryGenerator implements Callable<Trajectory> {
 
 	// These are our generators/evaluators
 	protected Policy _policy;
@@ -27,28 +29,56 @@ public abstract class TrajectoryGenerator {
 	protected Trajectory _trajectory;
 	protected State _currentState;
 	
+	/**
+	 * Construct a TrajectoryGenerator from a StateGenerator, Policy, and RewardFunction. This
+	 * method makes copies of the arguments to use.
+	 * @param gen - A StateGenerator to copy
+	 * @param policy - A Policy to copy
+	 * @param reward - A RewardFunction to copy
+	 */
 	public TrajectoryGenerator(StateGenerator gen, Policy policy, RewardFunction reward) {
 		_stateGen = gen.copy();
 		_policy = policy.copy();
 		_rewardFunc = reward.copy();
 	}
 	
+	/**
+	 * Copy constructor
+	 * @param other - TrajectoryGenerator to copy
+	 */
 	public TrajectoryGenerator(TrajectoryGenerator other) {
 		_stateGen = other._stateGen.copy();
 		_policy = other._policy.copy();
 		_rewardFunc = other._rewardFunc.copy();
 	}
 	
-	// TODO Reduce boilerplate by using reflection
-	// Return a deep copy of this object
+	/**
+	 * Create a deep copy of this generator.
+	 * @return A deep copy of this generator.
+	 */
 	public abstract TrajectoryGenerator copy();
 	
-	// Generates a trajectory and returns it. This may return the same
-	// trajectory in subsequent calls.
-	abstract public Trajectory generate_trajectory();
+	/**
+	 * Returns a generated Trajectory
+	 */
+	public Trajectory call() {
+		return get_trajectory();
+	}
 	
-	/** Initializes the current state to a starting state and resets the
-	 * Trajectory record.
+	/**
+	 * Generate a trajectory using the respective policy starting from a state produced
+	 * by the state generator with rewards from the reward function.
+	 * @return A deep copy of the generated trajectory.
+	 */
+	public Trajectory get_trajectory() {
+		initialize();
+		generate_trajectory();
+		return new Trajectory(_trajectory);
+	}
+	
+	/**
+	 * Initialize the generator by resetting internal fields and generating a new 
+	 * starting state.
 	 */
 	protected void initialize() {
 		if(_stateGen != null) {
@@ -59,7 +89,9 @@ public abstract class TrajectoryGenerator {
 		_trajectory._trajectory.clear();
 	}
 	
-	// Applies the specified action to the trajectory
+	/**
+	 * Execute the policy for one step.
+	 */
 	protected void step() {
 		
 		// Record history and take a step
@@ -70,8 +102,9 @@ public abstract class TrajectoryGenerator {
 		action.apply(_currentState); // This modifies _currentState!
 	}
 	
-	protected Trajectory get_trajectory() {
-		return new Trajectory(_trajectory);
-	}
+	/**
+	 * Produce a local trajectory and save it in the local _trajectory.
+	 */
+	abstract protected void generate_trajectory();
 	
 }
