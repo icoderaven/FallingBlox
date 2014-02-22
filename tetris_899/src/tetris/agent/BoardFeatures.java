f137c80f34c676c241bf0e9c8a62e2c194ab084c
eavrunin <eavrunin@cs.cmu.edu>
2014-02-22 16:22:03 -0500

    Debugged BoardFeatures

--- tetris_899/src/tetris/agent/BoardFeatures.java
+++ tetris_899/src/tetris/agent/BoardFeatures.java
@@ -1,7 +1,9 @@
 package tetris.agent;
 
 import tetris.simulator.*;
+
 import org.ejml.simple.SimpleMatrix;
+
 import java.util.*;
 
 
@@ -12,45 +14,55 @@ import java.util.*;
 
 public class BoardFeatures implements Feature {
 	
-    public SimpleMatrix get_feature_vector(State s, Action a){
-	s.makeMove(a.index);
-	board = new SimpleMatrix(s.getField());
-
-	// Set all nonzero values of board to 1
-	for(int i=0; i < s.ROWS; i++) {
-	    for(int j=0; j < s.COLS; j++) {
-		if(board.get(i, j) != 0) {
-		    board.set(i, j, 1);
+	public int nFeatures;
+	
+    public SimpleMatrix get_feature_vector(State s, Action a)
+    {
+    	// Look at the board after taking the action
+    	s.makeMove(a.index);
+    	
+    	// Extract the board and set all nonzero values to 1
+		SimpleMatrix board = new SimpleMatrix(State.COLS*State.ROWS, 1) ;
+		int[][] field = s.getField();
+		for(int i=0; i< s.ROWS; i++)
+		{
+			for(int j=0; j< s.COLS; j++)
+			{
+				board.set(i*board.numCols()+j ,  Math.min(field[i][j], 1));
+			}
 		}
-	    }
-	}
 
-	// Height of tallest column
-	maxH = Arrays.sort(s.getTop())[s.COLS-1];
+		// Compute the features:
+		
+		// Height of tallest column
+		int[] temp = s.getTop();
+		Arrays.sort(temp);
+		int maxH = temp[temp.length-1];
 
-	// Number of "holes" -- empty slots with a full slot higher in the col
-	// Number of empty slots below the top row
-	colHeight = s.getTop();
-	nHoles = 0;
-	nEmptyBelow = 0;
-	for(int i=0; i < s.COLS; i++) {
-	    h = colHeight[i];
-	    nFull = board.extractMatrix(0, h, i-1, i).elementSum();
-	    nHoles += h - nFull;
-	    nEmptyBelow += maxH - nFull;
-	}
+		// Number of "holes" -- empty slots with a full slot higher in the col
+		// Number of empty slots below the top row
+		int[] colHeight = s.getTop();
+		int nHoles = 0;
+		int nEmptyBelow = 0;
+		for(int i=0; i < s.COLS; i++) {
+			int h = colHeight[i];
+			int nFullCol = (int) board.extractMatrix(0, h, i-1, i).elementSum();
+			nHoles += h - nFullCol;
+			nEmptyBelow += maxH - nFullCol;
+		}
 
 
-	// Average column height?  Minimum column height?
-	// Average # of filled spaces in a row?  Number in top row?
-
-	res = [maxH];
-	return SimpleMatrix(res);
+		// Average column height?  Minimum column height?
+		// Average # of filled spaces in a row?  Number in top row?
+		nFeatures = 3;
+		SimpleMatrix res = new SimpleMatrix(nFeatures, 1);
+		res.setRow(0, maxH, nHoles, nEmptyBelow);
+		return res;
     }
 
 	// TODO Update as necessary
 	public int get_feature_dimension() {
-		return 1;
+		return nFeatures;
 	}
 
 	// TODO Update as necessary
