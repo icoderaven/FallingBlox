3d709f6775baf9986b0aac382e22f2d3ee3f69b8
Humphrey Hu <humphrey.hu@gmail.com>
2014-02-26 22:39:46 -0500

    Bugfix to covariance calculation.

--- tetris_899/src/tetris/agent/GradientPolicy.java
+++ tetris_899/src/tetris/agent/GradientPolicy.java
@@ -57,9 +57,12 @@ public class GradientPolicy implements Policy {
 		SimpleMatrix dist = pi(curr_state);
 		int index = dist.numRows()-1;
 		
+//		System.out.println("PDF:");
+//		dist.transpose().print();
+		
 		//Get a random number sampled from the uniform distribution  
 		double u = Math.random(), cdf = 0.0;
-		
+		dist = pi(curr_state);
 		//Move till the running total exceeds this probability value
 		for(int i=0; i<dist.numRows(); i++){
 			cdf+=dist.get(i);
@@ -185,95 +188,60 @@ public class GradientPolicy implements Policy {
 //	}
 	
 	public void fit_policy(Trajectory[] t_list, double step_size) {
-		SimpleMatrix gradSum = new SimpleMatrix(_params.numRows(), 1);
-		SimpleMatrix trajGradSum = new SimpleMatrix(_params.numRows(), 1);
-		SimpleMatrix gradCovs = new SimpleMatrix(_params.numRows(), _params.numRows());
+		SimpleMatrix deltaSum = new SimpleMatrix(_params.numRows(), 1);
+		SimpleMatrix covSum = new SimpleMatrix(_params.numRows(), _params.numRows());
 		
-		gradCovs.set(0);
+		deltaSum.set(0);
+		covSum.set(0);
 		
 		// Calculate optimal baselines
 //		SimpleMatrix baselines = calculate_baselines(t_list);
 //		baselines.transpose().print();
 		
-		int contrib = 0;
+		SimpleMatrix z = new SimpleMatrix( _params.numRows(), 1 );
+		SimpleMatrix delta = new SimpleMatrix( _params.numRows(), 1 );
+		SimpleMatrix covs = new SimpleMatrix( _params.numRows(), _params.numRows() );
+		
 		for(int i=0; i<t_list.length; i++)
 		{
-			trajGradSum.set(0);
+			z.set(0);
+			delta.set(0);
+			covs.set(0);
+			Trajectory traj = t_list[i];
 			
-			double discount = 1.0;
-			for(int j=0; j < t_list[i].tuples.size(); j++)
+			for(int t = 0; t < traj.tuples.size(); t++)
 			{
-				SARTuple sar = t_list[i].tuples.get(j);
+				SARTuple sar = traj.tuples.get(t);
 				SimpleMatrix grad = gradient(sar.state, sar.action);
 				
-//				System.out.format("Step %d gradient:%n", j);
-//				grad.transpose().print();
+				// z_t+1 = gamma*z_t + grad
+				z = z.scale(_gamma).plus( grad );
 				
-				trajGradSum = trajGradSum.plus(grad);
+				// delta_t+1 = delta_t*(t/(t+1)) + r_t+1 * z_t+1 / t+1
+				delta = delta.scale( 1.0*t/( t + 1.0 ) ).plus( z.scale( sar.reward/( t + 1.0 ) ) );
 				
-				SimpleMatrix discountedReward = new SimpleMatrix(_params.numRows(), 1);
-				discountedReward.set( sar.reward );
-
-//				System.out.format("Step %f reward:%n", sar.reward );
+				covs = covs.plus( grad.mult( grad.transpose() ) ); 
 				
-//				SimpleMatrix advantage = discountedReward.minus( baselines );
-				SimpleMatrix advantage = discountedReward;
-				
-				SimpleMatrix gradContribution = trajGradSum.elementMult( advantage );
-//				System.out.format("Step %d grad contribution:%n", j);
-//				gradContribution.transpose().print();
-				
-				trajGradSum = trajGradSum.scale(_gamma);
-				gradSum = gradSum.plus( gradContribution );
-				
-				// Keep a running total of the outer products for covariance calculation later
-				gradCovs = gradCovs.plus( grad.mult( grad.transpose() ) );
-				
-				// Instantaneous reward, non-discounted
-//				delta = delta.plus( grad.scale( t.tuples.get(j).reward) );
-//				System.out.format("Reward at step %d: %f%n", j, t.tuples.get(j).reward);
-				
-				// Average reward, non-discounted
-//				delta = delta.plus( grad.scale( averageReward ));
-				
-				// Reward to go, discounted
-//				delta = delta.plus( grad.scale( t.sum_rewards_tail(j, _gamma) ) );
-				
-				// Running average reward, discounted
-//				z = z.scale(gamma).plus(grad);
-				//delta_{t+1} = delta + (1/t+1)(r_{t+1}*z_{t+1} - delta)
-//				delta = delta.plus(1.0/(j+2), z.scale(t.sum_rewards(j, 1.0)).minus(delta));
-//				System.out.println("Delta running average:");
-//				delta.transpose().print();
-				
-				contrib++;
-				discount = discount * _gamma;
 			}
-
+			deltaSum = deltaSum.plus( delta.scale( 1.0/traj.tuples.size() ) );
+			covSum = covSum.plus( covs.scale( 1.0/traj.tuples.size() ) );
 		}
 		
-		// Averaged over each time in each trajectory
-		gradSum = gradSum.scale(1.0/contrib);
-		gradCovs = gradCovs.scale(1.0/contrib);
+		// Averaged over each trajectory
+		deltaSum = deltaSum.scale( 1.0/t_list.length );
+		covSum = covSum.scale( 1.0/t_list.length );
 		
 		// Calculate information matrix
-		gradCovs = gradCovs.plus( SimpleMatrix.identity(gradCovs.numRows()).scale(1E-3) ); // Hack smoothing
-//		for(int i = 0; i < gradCovs.numRows(); i++) {
-//			if(gradCovs.get(i,i) < 1E-3) {
-//				gradCovs.set(i, i, 1.0);
-//			}
-//		}
-		SimpleMatrix gradInfo = gradCovs.invert();
+		covSum = covSum.plus( SimpleMatrix.identity(covSum.numRows()).scale(1E-12) ); // Hack smoothing
+		SimpleMatrix info = covSum.invert();
 		
 		// Scale delta by info matrix
-		SimpleMatrix grad = gradInfo.mult(gradSum);
-//		SimpleMatrix grad = gradSum;
+		SimpleMatrix grad = info.mult(deltaSum);
 		
 		//Step params in this direction
 		double step = step_size;
 		_params = _params.plus(step, grad);
 //		normalize_params();
-		
 		System.out.format("Step size: %f%n", Math.sqrt(grad.normF()) );
 	}
 
@@ -293,7 +261,9 @@ public class GradientPolicy implements Policy {
 		SimpleMatrix probs = new SimpleMatrix(moves.length, 1);
 
 		// Get all exponents first
+		double sum = 0.0;
 		double maxVal = Double.NEGATIVE_INFINITY;
+		int numValid = 0;
 		for (int a_prime = 0; a_prime < moves.length; a_prime++) {
 			
 			double logLikelihood = calculate_log_likelihood(s, new Action(a_prime));
@@ -302,8 +272,15 @@ public class GradientPolicy implements Policy {
 			if(logLikelihood > maxVal) {
 				maxVal = logLikelihood;
 			}
+			
+			if(logLikelihood != Double.NEGATIVE_INFINITY) {
+				sum += logLikelihood;
+				numValid++;
+			}
 			exponents.set(a_prime, logLikelihood);
 		}
+		
+//		exponents.transpose().print();
 		
 		// If all moves are fatal, return uniform
 		if(maxVal == Double.NEGATIVE_INFINITY) {
@@ -312,8 +289,10 @@ public class GradientPolicy implements Policy {
 		}
 		
 		// Calculate probabilities and track sum
+		double meanVal = sum/numValid;
 		for (int a_prime = 0; a_prime < moves.length; a_prime++) {
-			double normalizedExponent = exponents.get(a_prime) - maxVal;
+			double normalizedExponent = exponents.get(a_prime) - maxVal + 10.0;
+//			double normalizedExponent = exponents.get(a_prime) - meanVal;
 			double likelihood = Math.exp(normalizedExponent);
 			probs.set(a_prime, likelihood);
 		}
@@ -416,10 +395,11 @@ public class GradientPolicy implements Policy {
 	}
 	
 	private void normalize_params() {
-		double mean = _params.elementSum()/_params.numRows();
-		SimpleMatrix shift = new SimpleMatrix( _params.numRows(), 1 );
-		shift.set(mean);
-		_params = _params.minus( shift );
+		_params = _params.scale( 1.0/ Math.sqrt(_params.normF()) );
+//		double mean = _params.elementSum()/_params.numRows();
+//		SimpleMatrix shift = new SimpleMatrix( _params.numRows(), 1 );
+//		shift.set(mean);
+//		_params = _params.minus( shift );
 	}
 	
 	@Override
